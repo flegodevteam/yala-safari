7bed58288ed2839aa7f2dfade3774e575f0616dc
chinthakabandara710 <77668640+chinthakabandara710@users.noreply.github.com>
2025-08-14 14:25:58 +0530

    Update Server.js

--- backend/Server.js
+++ backend/Server.js
@@ -50,6 +50,10 @@ app.get("/", (req, res) => {
 
 // Start the server
 const PORT = process.env.PORT || 5000;
+
 app.listen(PORT, () => {
   console.log(`Server running on http://localhost:${PORT}`);
 });
+
+
+module.exports = app;
